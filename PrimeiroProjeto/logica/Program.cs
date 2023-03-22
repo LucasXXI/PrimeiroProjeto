b4950f56e067995e87014ec129b40a58fd93bc38
LucasXXI <lucasleal2001@gmail.com>
2023-03-22 15:31:54 -0300

    feat: added struct and array exercises;

--- PrimeiroProjeto/logica/Program.cs
+++ PrimeiroProjeto/logica/Program.cs
@@ -4,6 +4,7 @@ using PrimeiroProjeto.poo.exerciciosClasses;
 using PrimeiroProjeto.poo.construtores;
 using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
+using PrimeiroProjeto.poo.Structs;
 
 namespace PrimeiroProjeto
 {
@@ -11,63 +12,26 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            Banco banco;
+            Console.WriteLine("Quantas alturas voce quer adicionar?");
+            int n = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("-----BANCO----");
-            Console.Write("Entre o numero da conta: ");
-            int numero = int.Parse(Console.ReadLine());
+            double[] vect = new double[n];
 
-            Console.Write("Entre o Titular da conta: ");
-            string titular = Console.ReadLine();
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Digite uma Altura: ");
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.Write("Haverá Deposito inicial? (s/n) ");
-            char c = char.Parse(Console.ReadLine());
-
-                if (c == 's' || c == 'S')
-                {
-                    Console.Write("Entre o valor do deposito: ");
-                    double valorDep = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    banco = new Banco(numero, titular, valorDep);
-                    
-
-                    Console.WriteLine("Dados da conta:");
-                    Console.WriteLine(banco);
-
-                    Console.WriteLine();
-                    Console.WriteLine("Entre um valor para deposito: ");
-                    valorDep = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    banco.Deposito(valorDep);
-                    Console.WriteLine("Dados da conta atualizados: ");
-                    Console.Write(banco);
-
-                    Console.WriteLine();
-                    Console.Write("Entre um valor para saque: ");
-                    double valSaq = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    banco.Saque(valSaq);
-                    Console.WriteLine("Dados da conta atualizados: ");
-                    Console.Write(banco);
-                }
-                else if (c == 'n' || c == 'N')
-                {
-                    banco = new Banco(numero, titular);
-                    Console.WriteLine("Dados da conta:");
-                    Console.WriteLine(banco);
-                    
-
-                    Console.WriteLine();
-                    Console.WriteLine("Entre um valor para deposito: ");
-                    double valorDep = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    banco.Deposito(valorDep);
-                    Console.WriteLine("Dados da conta atualizados: ");
-                    Console.Write(banco);
-
-                    Console.WriteLine();
-                    Console.Write("Entre um valor para saque: ");
-                    double valSaq = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    banco.Saque(valSaq);
-                    Console.WriteLine("Dados da conta atualizados: ");
-                    Console.Write(banco);
-                }
             }
+
+            foreach(double element in vect)
+            {
+                Console.WriteLine(element.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            double avg = vect.Sum() / n;
+
+            Console.Write("AVARAGE HEIGHT: " + avg.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
+}
---
+++ PrimeiroProjeto/poo/Structs/Struct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto.poo.Structs
+{
+    struct Point
+    {
+        public double x, y;
+
+        public override string ToString()
+        {
+            return "(" + x + "," + y + ")"; 
+        }
+    }
+}
