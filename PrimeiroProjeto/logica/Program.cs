4d71bd4bc5e9c126f178b21d889140594df7c324
LucasXXI <lucasleal2001@gmail.com>
2023-03-23 16:14:21 -0300

    feat: added list and array exemples, also some list methods recap

--- PrimeiroProjeto/logica/Program.cs
+++ PrimeiroProjeto/logica/Program.cs
@@ -3,7 +3,6 @@ using PrimeiroProjeto.poo;
 using PrimeiroProjeto.poo.exerciciosClasses;
 using PrimeiroProjeto.poo.construtores;
 using System.Globalization;
-using System.Security.Cryptography.X509Certificates;
 using PrimeiroProjeto.poo.Structs;
 using PrimeiroProjeto.vetores;
 
@@ -13,39 +12,73 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            Hotel[] reserva = new Hotel[10]; 
+            List<string> list = new List<string>();
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Lucas");
+            list.Add("Carla");
+            list.Add("Anna");
+            list.Insert(0, "Dracula");
 
-            Console.WriteLine("Serão quantas reservas?");
-            int r = int.Parse(Console.ReadLine());
-
-
-            for(int i  = 1; i <= r; i++)
+            foreach (var element in list)
             {
-                Console.WriteLine($"Aluguel #{i}:");
-                Console.Write("Nome: ");
-                string cliente = Console.ReadLine();
-
-                Console.Write("Email: ");
-                string email = Console.ReadLine();
-
-                Console.Write("Quarto: ");
-                int quarto = int.Parse(Console.ReadLine());
-
-                reserva[quarto] = new Hotel(cliente, email);
+                Console.WriteLine(element);
             }
 
+            Console.WriteLine("List count: " + list.Count());
+            string s1 = list.Find(x => x[0] == 'A'); //funcao lambda
+            Console.WriteLine("Teste: " + s1);
 
-            Console.WriteLine();
-            Console.WriteLine("Quartos Ocupados: ");
-            for(int i = 0; i < 10;  i++)
+            string s2 = list.FindLast(x => x[0] == 'A');
+            Console.WriteLine("Last: " + s2);
+
+            int p1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine(p1);
+
+            int p2 = list.FindLastIndex(x  => x[0] == 'A');
+            Console.WriteLine(p2);
+
+            Console.WriteLine("---------------");
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+
+            foreach(var element in list2)
             {
-                if (reserva[i] != null)
-                {
-                    Console.WriteLine(i + ": " + reserva[i]);
-                }
+                Console.WriteLine(element);
             }
 
+            Console.WriteLine("---------------");
+            list.Remove("Alex");
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("---------------");
+            list.RemoveAll(x => x[0] == 'M');
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("---------------");
+            list.RemoveAt(2);
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("---------------");
+            list.RemoveAll(x => x[0] == 'D');
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+            //pode ser passaado no find (forma explicita);
+            static bool a(string s)
+            {
+                return s[0] == 'A';
+            }
         }
-    }
 
+    }
 }
