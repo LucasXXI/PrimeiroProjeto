d1d286ff88db2a077253951a41f0b20a1872ee22
LucasXXI <lucasleal2001@gmail.com>
2023-03-24 14:39:09 -0300

    feat: added list exemple

--- PrimeiroProjeto/logica/Program.cs
+++ PrimeiroProjeto/logica/Program.cs
@@ -12,71 +12,52 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            List<string> list = new List<string>();
-            list.Add("Maria");
-            list.Add("Alex");
-            list.Add("Bob");
-            list.Add("Lucas");
-            list.Add("Carla");
-            list.Add("Anna");
-            list.Insert(0, "Dracula");
+            Employee emp;
 
-            foreach (var element in list)
+            Console.Write("How many employees will be registered? ");
+            int x = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            for(int i = 1; i <= x; i++)
             {
-                Console.WriteLine(element);
+
+                Console.WriteLine($"Employee #{i}:");
+
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+
+                list.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+
             }
 
-            Console.WriteLine("List count: " + list.Count());
-            string s1 = list.Find(x => x[0] == 'A'); //funcao lambda
-            Console.WriteLine("Teste: " + s1);
+            Console.WriteLine("Enter the employee id that will have a salary increase: ");
+            int searchId = int.Parse(Console.ReadLine());
 
-            string s2 = list.FindLast(x => x[0] == 'A');
-            Console.WriteLine("Last: " + s2);
-
-            int p1 = list.FindIndex(x => x[0] == 'A');
-            Console.WriteLine(p1);
-
-            int p2 = list.FindLastIndex(x  => x[0] == 'A');
-            Console.WriteLine(p2);
-
-            Console.WriteLine("---------------");
-            List<string> list2 = list.FindAll(x => x.Length == 5);
-
-            foreach(var element in list2)
+            emp = list.Find(x => x.Id == searchId);
+            if(emp != null)
             {
-                Console.WriteLine(element);
+                Console.WriteLine("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine());   
+                emp.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
             }
 
-            Console.WriteLine("---------------");
-            list.Remove("Alex");
-            foreach (var element in list)
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees");
+            foreach(Employee element in list)
             {
                 Console.WriteLine(element);
-            }
-            Console.WriteLine("---------------");
-            list.RemoveAll(x => x[0] == 'M');
-            foreach (var element in list)
-            {
-                Console.WriteLine(element);
-            }
-
-            Console.WriteLine("---------------");
-            list.RemoveAt(2);
-            foreach (var element in list)
-            {
-                Console.WriteLine(element);
-            }
-
-            Console.WriteLine("---------------");
-            list.RemoveAll(x => x[0] == 'D');
-            foreach (var element in list)
-            {
-                Console.WriteLine(element);
-            }
-            //pode ser passaado no find (forma explicita);
-            static bool a(string s)
-            {
-                return s[0] == 'A';
             }
         }
 
